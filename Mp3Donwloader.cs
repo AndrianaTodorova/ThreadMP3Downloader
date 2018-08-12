a7cb3df82b3b8e9f50a7c3666dd1091f750e3c0e
AndrianaTodorova <rosiani@mail.bg>
2018-08-12 13:56:28 +0300

    mp3 downloader using threads

---
+++ Mp3Donwloader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DownloadMP3
+{
+    public class Mp3Donwloader
+    {
+
+        public Mp3Donwloader()
+        {
+            this.ProccessedTasks = new Dictionary<string, Task>();
+        }
+
+        public IDictionary<string, Task> ProccessedTasks { get; set; }
+
+        public IEnumerable<string> GetContentUrls(string filepath)
+        {
+            //Gets the content from the specified filePath
+            return File.ReadAllLines(filepath).ToList();
+
+        }
+
+        public void Download(string uri)
+        {
+            //Regex for the name of the mp3 files
+            Regex reg = new Regex(@"(?:.*\/)(.*?[.]+[a-z0-9]{2,4}$)");
+            string fileName = reg.Match(uri).Groups[1].Value.Replace("%20", " ");
+            var task = Task.Run(() => this.DownloadMP3(uri));
+            var awaiter = task.GetAwaiter();
+            //Wneh a task is completed gets the result of it and then writes its content with a fileName specified
+            awaiter.OnCompleted(() => this.WriteDataToFile(fileName, awaiter.GetResult()));
+            this.ProccessedTasks.Add(fileName, task);
+        }
+
+        private void WriteDataToFile(string fileName, byte[] content)
+        {
+            File.WriteAllBytes($"{fileName}", content);
+            Console.WriteLine($"{fileName} downloaded");
+        }
+
+        private byte[] DownloadMP3(string uri)
+        {
+            //Using WebClient class for downloading data from specified URL
+            using (WebClient client = new WebClient())
+            {
+                return client.DownloadData(uri);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace DownloadMP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string DIRECTORY = @"DIRECTORY PATH";
+            var files = Directory.GetFiles(DIRECTORY);
+
+            //Get the time elapsed using couple of threads
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            //Instance of the class which contains the main logic for downloading easily mp3 music
+            Mp3Donwloader downloader = new Mp3Donwloader();
+            TraverseFiles(files, downloader);
+
+            while (downloader.ProccessedTasks.Count > 0)
+            {
+                //List containing each song name from the finished tasks
+                IList<string> finishedTask = (from downloaderProccessedTask
+                    in downloader.ProccessedTasks
+                                              where downloaderProccessedTask.Value.IsCompletedSuccessfully
+                                              select downloaderProccessedTask.Key).ToList();
+
+                RemoveFinishedTask(downloader, finishedTask);
+            }
+            watch.Stop();
+            // Write result.
+            Console.WriteLine("Time elapsed: {0}", watch.Elapsed);
+        }
+
+        private static void RemoveFinishedTask(Mp3Donwloader downloader, IList<string> finishedTask)
+        {
+            foreach (var finishT in finishedTask)
+            {
+                downloader.ProccessedTasks.Remove(finishT);
+            }
+        }
+
+        private static void TraverseFiles(string[] files, Mp3Donwloader downloader)
+        {
+            foreach (var file in files)
+            {
+                TraverseUrls(downloader, file);
+            }
+        }
+
+        private static void TraverseUrls(Mp3Donwloader downloader, string file)
+        {
+            foreach (var contentUrl in downloader.GetContentUrls(file))
+            {
+                downloader.Download(contentUrl);
+            }
+        }
+    }
+}
